cf64000267208019572f1c85ac5bc5b03fb74d3b
diegovinci <diego.vinci@outlook.com>
2021-06-12 15:23:59 -0300

    feat: add UserInformation component

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,6 @@
-import Head from 'next/head';
-import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment'
-import PageTitle from 'ui/components/data-display/PageTitle/PageTitle'
+import SafeEnvironment from 'ui/components/feedback/SafeEnvironment/SafeEnvironment';
+import PageTitle from 'ui/components/data-display/PageTitle/PageTitle';
+import UserInformation from 'ui/components/data-display/UserInformation/UserInformation';
 
 export default function Home() {
   return (
@@ -8,7 +8,22 @@ export default function Home() {
       <SafeEnvironment />
       <PageTitle
         title={'Conheça os profissionais'}
-        subtitle={'Preencha seu endereço e veja todos os profissionais da sua localidade'}
+        subtitle={
+          'Preencha seu endereço e veja todos os profissionais da sua localidade'
+        }
+      />
+
+      <UserInformation
+        name={'Diego'}
+        picture={'https://github.com/diegovinci.png'}
+        rating={3}
+        description={'São Paulo'}
+      />
+      <UserInformation
+        name={'Akira'}
+        picture={'https://github.com/hanashiro.png'}
+        rating={4}
+        description={'São Paulo'}
       />
     </div>
   );
