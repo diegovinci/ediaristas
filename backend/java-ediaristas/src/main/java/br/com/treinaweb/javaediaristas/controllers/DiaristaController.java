caa67b38a12f9104e943976ac1bb790d8f3438fc
diegovinci <diego.vinci@outlook.com>
2021-10-12 18:21:03 -0300

    feat: add action for diaristas registration

--- backend/java-ediaristas/src/main/java/br/com/treinaweb/javaediaristas/controllers/DiaristaController.java
+++ backend/java-ediaristas/src/main/java/br/com/treinaweb/javaediaristas/controllers/DiaristaController.java
@@ -1,15 +1,21 @@
 package br.com.treinaweb.javaediaristas.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.com.treinaweb.javaediaristas.models.Diarista;
+import br.com.treinaweb.javaediaristas.repositories.DiaristaRepository;
 
 @Controller
 @RequestMapping("/admin/diaristas")
 public class DiaristaController {
+
+    @Autowired
+    private DiaristaRepository repository;
 
     @GetMapping("/cadastrar")
     public ModelAndView cadastrar() {
@@ -19,4 +25,12 @@ public class DiaristaController {
 
         return modelAndView;
     }
+
+    @PostMapping("/cadastrar")
+    public String cadastrar(Diarista diarista) {
+        repository.save(diarista);
+
+        return "redirect:/admin/diaristas/cadastrar";
+    }
 }
+
