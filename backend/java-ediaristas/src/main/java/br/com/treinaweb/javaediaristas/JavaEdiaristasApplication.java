99a6a6775fb5a7e78713db8047bf5e64e02a859c
diegovinci <diego.vinci@outlook.com>
2021-10-12 16:18:47 -0300

    feat: add java-ediaristas project initial structure

---
+++ backend/java-ediaristas/src/main/java/br/com/treinaweb/javaediaristas/JavaEdiaristasApplication.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.javaediaristas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEdiaristasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEdiaristasApplication.class, args);
+	}
+
+}
---
+++ backend/java-ediaristas/src/test/java/br/com/treinaweb/javaediaristas/JavaEdiaristasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.javaediaristas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEdiaristasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
