792952c3b91c1bac9fabf8433b7958c0f1bf2f8f
diegovinci <diego.vinci@outlook.com>
2021-10-16 15:14:36 -0300

    feat: add filter by cep and ibge code in diaristas rest controller

--- backend/java-ediaristas/src/main/java/br/com/treinaweb/javaediaristas/controllers/DiaristaRestController.java
+++ backend/java-ediaristas/src/main/java/br/com/treinaweb/javaediaristas/controllers/DiaristaRestController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.treinaweb.javaediaristas.repositories.DiaristaRepository;
+import br.com.treinaweb.javaediaristas.services.ViaCepService;
 import br.com.treinaweb.javaediaristas.models.Diarista;
 
 @RestController
@@ -17,8 +19,14 @@ public class DiaristaRestController {
     @Autowired
     private DiaristaRepository repository;
 
+    @Autowired
+    private ViaCepService viaCepService;
+
     @GetMapping
-    public List<Diarista> buscarDiaristasPorCep() {
-        return repository.findAll();
+    public List<Diarista> buscarDiaristasPorCep(@RequestParam String cep) {
+        var endereco = viaCepService.buscarEnderecoPorCep(cep);
+        var codigoIbge = endereco.getIbge();
+        
+        return repository.findByCodigoIbge(codigoIbge);
     }
 }
--- backend/java-ediaristas/src/main/java/br/com/treinaweb/javaediaristas/repositories/DiaristaRepository.java
+++ backend/java-ediaristas/src/main/java/br/com/treinaweb/javaediaristas/repositories/DiaristaRepository.java
@@ -1,9 +1,13 @@
 package br.com.treinaweb.javaediaristas.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.treinaweb.javaediaristas.models.Diarista;
 
 public interface DiaristaRepository extends JpaRepository<Diarista, Long> {
     
+    List<Diarista> findByCodigoIbge(String codigoIbge);
+
 }
